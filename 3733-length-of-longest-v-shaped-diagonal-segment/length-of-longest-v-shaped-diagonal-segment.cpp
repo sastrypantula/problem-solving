9dcca5273e482980f4c9c49f28523db8522bb8dd
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2025-08-27 22:19:20 +0530

    Time: 276 ms (82.89%) | Memory: 104 MB (67.91%) - LeetSync

--- 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
@@ -1,19 +1,67 @@
+// class Solution {
+// public:
+// vector<vector<int>> dirs={{1,1},{1,-1},{-1,-1},{-1,1}};
+// int m;
+// int n;
+// int solve(int i,int j,int d,bool canTurn,int val,vector<vector<int>>& grid){
+//   int i_ = i + dirs[d][0];
+//         int j_ = j + dirs[d][1];
+
+//         if(i_ < 0 || i_ >= m || j_ < 0 || j_ >= n || grid[i_][j_] != val) {
+//             return 0;
+//         }
+
+//        
+//         int result = 0;
+//         int keepMoving = 1 + solve(i_, j_, d, canTurn, val == 2 ? 0 : 2, grid);
+//         result = max(result, keepMoving);
+
+//         if(canTurn == true) {
+//             int turnAndMove = max(keepMoving, 1 + solve(i_, j_, (d+1)%4, false, val == 2 ? 0 : 2, grid));
+//             result = max(result, turnAndMove);
+//         }
+
+//         return result;
+
+
+// }
+//     int lenOfVDiagonal(vector<vector<int>>& grid) {
+//          m=grid.size();
+//          n=grid[0].size();
+//         int result=0;
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(grid[i][j]==1){
+//                     for(int d=0;d<4;d++){
+//                     result=max(result,1+solve(i,j,d,true,2,grid));
+//                     }
+//                 }
+//             }
+//         }
+//         return result;
+//    }
+// };
+
+// with dp 
+
 class Solution {
 public:
-vector<vector<int>> dirs={{1,1},{1,-1},{-1,-1},{-1,1}};
-int m;
-int n;
-int solve(int i,int j,int d,bool canTurn,int val,vector<vector<int>>& grid){
-  int i_ = i + dirs[d][0];
-        int j_ = j + dirs[d][1];
+
+    vector<vector<int>> directions = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
+    int m, n;
+    int t[501][501][4][2];
+
+    int solve(int i, int j, int d, bool canTurn, int val, vector<vector<int>>& grid) {
+        int i_ = i + directions[d][0];
+        int j_ = j + directions[d][1];
 
         if(i_ < 0 || i_ >= m || j_ < 0 || j_ >= n || grid[i_][j_] != val) {
             return 0;
         }
 
-        // if(t[i_][j_][d][canTurn] != -1) {
-        //     return t[i_][j_][d][canTurn];
-        // }
+        if(t[i_][j_][d][canTurn] != -1) {
+            return t[i_][j_][d][canTurn];
+        }
 
         int result = 0;
         int keepMoving = 1 + solve(i_, j_, d, canTurn, val == 2 ? 0 : 2, grid);
@@ -24,23 +72,27 @@ int solve(int i,int j,int d,bool canTurn,int val,vector<vector<int>>& grid){
             result = max(result, turnAndMove);
         }
 
-        return result;
+        return t[i_][j_][d][canTurn] = result;
+    }
 
-
-}
     int lenOfVDiagonal(vector<vector<int>>& grid) {
-         m=grid.size();
-         n=grid[0].size();
-        int result=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
-                    for(int d=0;d<4;d++){
-                    result=max(result,1+solve(i,j,d,true,2,grid));
+        m = grid.size();
+        n = grid[0].size();
+        memset(t, -1, sizeof(t));
+
+        int result = 0;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 1) {
+                    for(int d = 0; d <= 3; d++) {
+                        result = max(result, 1 + solve(i, j, d, true, 2, grid));
                     }
                 }
             }
         }
+
         return result;
-   }
+
+    }
 };

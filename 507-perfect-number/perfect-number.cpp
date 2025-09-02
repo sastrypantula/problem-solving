450d9c40a01e5fce7816ffb16dc5a3dced8d24c7
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2025-09-02 18:19:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (57.22%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        int sum=0;
-        for(int i=1;i<=num-1;i++){
+        // int sum=0;
+        // for(int i=1;i<=num-1;i++){
+        //     if(num%i==0){
+        //         sum+=i;
+        //     }
+        // }
+        // if(num>0 && sum==num){
+        //     return true;
+        // }
+        // return false;
+        if(num==1) return false;
+        int sum =1;
+        for(int i=2;i<=sqrt(num);i++){
             if(num%i==0){
-                sum+=i;
+                sum+=(i+(num/i));
             }
         }
-        if(num>0 && sum==num){
+        if(sum==num){
             return true;
         }
-        return false;
+        else return false;
     }
 };

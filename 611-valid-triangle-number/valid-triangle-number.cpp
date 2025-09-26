148fb30a261d826ad08b422e8b9dd1192f036d01
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2025-09-26 20:41:17 +0530

    Time: 33 ms (49.07%) | Memory: 16.7 MB (42.88%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -27,16 +27,33 @@ int binsearch(vector<int>& nums,int sum,int low,int high){
         //     }
         //  }
 
-        ///....... BINARYSEARCH + SORTING................
+        ///....... BINARYSEARCH + SORTING................O(n^2 * log n)
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<n-2;i++){
+        //     for(int j=i+1;j<n-1;j++){
+        //         int sum=nums[i]+nums[j];
+        //        int ans= binsearch(nums,sum,j+1,n-1);
+        //         count+=ans-j-1;
+        //     }
+        // }
+        /////// Two pointer O(nsq)
+        if(n<3){
+            return 0;
+        }
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n-2;i++){
-            for(int j=i+1;j<n-1;j++){
-                int sum=nums[i]+nums[j];
-               int ans= binsearch(nums,sum,j+1,n-1);
-                count+=ans-j-1;
+        for(int k=n-1;k>=2;k--){
+            int j=k-1;
+            int i=0;
+            while(i<j){
+                if(nums[i]+nums[j]>nums[k]){
+                    count+=j-i;
+                    j--;
+                }
+                else{
+                    i++;
+                }
             }
         }
-    
 
          return count;
     }

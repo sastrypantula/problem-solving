e35d8b4b8f7c349b5eb12a077253c6a163633c8f
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2025-10-28 17:06:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (87.68%) - LeetSync

--- 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.cpp
+++ 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
     long long removeZeros(long long n) {
-        string ans=to_string(n);
-        string res="";
-        for(int i=0;i<ans.length();i++){
-            if(ans[i]!='0'){
-                res=res+ans[i];
+        // string ans=to_string(n);
+        // string res="";
+        // for(int i=0;i<ans.length();i++){
+        //     if(ans[i]!='0'){
+        //         res=res+ans[i];
+        //     }
+        // }
+        // return stoll(res);
+         long long result = 0, place = 1;
+
+        while (n > 0) {
+            int digit = n % 10;
+            if (digit != 0) {
+                result += digit * place;
+                place *= 10;
             }
+            n /= 10;
         }
-        return stoll(res);
+
+        return result;
     }
 };

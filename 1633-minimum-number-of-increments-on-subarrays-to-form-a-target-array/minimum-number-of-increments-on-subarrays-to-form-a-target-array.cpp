149e8159589e699a439cc124b0de0f47554061e5
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2025-10-30 13:12:01 +0530

    Time: 0 ms (100.00%) | Memory: 77 MB (65.87%) - LeetSync

---
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minNumberOperations(vector<int>& target) {
+        int res=0;
+        int prev=0;
+        int curr=0;
+        for(int i=0;i<target.size();i++){
+            curr=target[i];
+            if(curr>0 && prev<0 || curr<0 && prev>0){
+                res+=abs(curr);
+            }
+            else if(curr>prev){
+                res+=abs(curr)-abs(prev);
+            }
+          
+            prev=curr;
+        }
+        return res;
+    }
+};

793928abfc99de8beca0ae99e01478237e237844
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2025-10-30 14:32:47 +0530

    Time: 8 ms (48.81%) | Memory: 31.6 MB (40.94%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -1,38 +1,50 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        if(nums.size()==3){
-            return nums[2]*nums[1]*nums[0];
-        }
-        int n=nums.size();
-        int res=1;
-        int count=0;
-        sort(nums.begin(),nums.end());
-        bool pos=true;
-        bool neg=true;
-        bool zero=true;
-        for(int it:nums){
-            if(it>0){
-                neg=false;
-                zero=false;
-            }
-            else if(it<0){
-                pos=false;
-                zero=false;
-            }
+//         if(nums.size()==3){
+//             return nums[2]*nums[1]*nums[0];
+//         }
+//         int n=nums.size();
+//         int res=1;
+//         int count=0;
+//         sort(nums.begin(),nums.end());
+//         bool pos=true;
+//         bool neg=true;
+//         bool zero=true;
+//         for(int it:nums){
+//             if(it>0){
+//                 neg=false;
+//                 zero=false;
+//             }
+//             else if(it<0){
+//                 pos=false;
+//                 zero=false;
+//             }
           
-            }
-cout<<"Positive : "<<pos<<endl;
-cout<<"Negative : "<<neg<<endl;
-cout<<"Zero : "<<zero<<endl;
-        if(neg==false && pos==true){
-              return nums[n-1]*nums[n-2]*nums[n-3];
-        }
-        if(neg==true && pos==false){
-              return nums[n-1]*nums[n-2]*nums[n-3];
-        }
-        int a=nums[0]*nums[1]*nums[n-1];
-        int b=nums[n-1]*nums[n-2]*nums[n-3];
-        return max(a,b);
+//             }
+// cout<<"Positive : "<<pos<<endl;
+// cout<<"Negative : "<<neg<<endl;
+// cout<<"Zero : "<<zero<<endl;
+//         if(neg==false && pos==true){
+//               return nums[n-1]*nums[n-2]*nums[n-3];
+//         }
+//         if(neg==true && pos==false){
+//               return nums[n-1]*nums[n-2]*nums[n-3];
+//         }
+//         int a=nums[0]*nums[1]*nums[n-1];
+//         int b=nums[n-1]*nums[n-2]*nums[n-3];
+//         return max(a,b);
+
+            sort(nums.begin(),nums.end());
+            int n = nums.size();
+        
+       
+        long long product1 = (long long)nums[n-1] * nums[n-2] * nums[n-3];
+        
+
+        long long product2 = (long long)nums[0] * nums[1] * nums[n-1];
+        
+    
+        return (int)max(product1, product2);
     }
 };

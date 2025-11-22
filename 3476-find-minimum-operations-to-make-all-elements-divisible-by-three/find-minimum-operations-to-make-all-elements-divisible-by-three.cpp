a4fa23f9137a5a8bd60a6dd9ba87d40b35944097
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2025-11-22 12:36:27 +0530

    Time: 0 ms (100.00%) | Memory: 23.3 MB (45.48%) - LeetSync

--- 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -2,22 +2,29 @@ class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
          int count=0;
-         cout<<"I am outside the loop"<<endl;
-         for(int it:nums){
-               cout<<"I am inside the loop"<<endl;
+        //  for(int it:nums){
+               
+        //     if(it%3==0){
+                
+        //         continue;
+        //     }
+        //     else if(((it-1)%3==0) || ((it+1)%3==0)){
+                
+        //         count+=1;
+        //     }
+        //     else if(((it-2)%3==0) || ((it+2)%3==0)){
+               
+        //         count+=2;
+        //     }
+        //  }
+        for(int it:nums){
             if(it%3==0){
-                cout<<"I am inside the continue loop"<<endl;
                 continue;
             }
-            else if(((it-1)%3==0) || ((it+1)%3==0)){
-                cout<<"I am inside the one loop"<<endl;
-                count+=1;
+            else{
+                count++;
             }
-            else if(((it-2)%3==0) || ((it+2)%3==0)){
-                cout<<"I am inside the two loop"<<endl;
-                count+=2;
-            }
-         }
+        }
          return count;
     }
 };

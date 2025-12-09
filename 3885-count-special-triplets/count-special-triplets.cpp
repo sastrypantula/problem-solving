0dd4e032bfd01af7bef8c852cecb93f9ccf8e3fa
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2025-12-09 18:40:55 +0530

    Time: 768 ms (63.14%) | Memory: 403 MB (55.47%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -2,22 +2,37 @@ class Solution {
 public:
 int MOD=1e9+7;
     int specialTriplets(vector<int>& nums) {
-        int n=nums.size();
-        unordered_map<int,int>left;
-        unordered_map<int,int>total;
-        for(int x:nums){
-            total[x]++;
-        }
-        int ans=0;
-        for(int x:nums){
+    //     int n=nums.size();
+    //     unordered_map<int,int>left;
+    //     unordered_map<int,int>total;
+    //     for(int x:nums){
+    //         total[x]++;
+    //     }
+    //     int ans=0;
+    //     for(int x:nums){
             
-             total[x]--;  
-    long long leftc = left[2 *x];
-    long long rightc = total[2 * x];
-    ans = (ans + (leftc* rightc) % MOD) % MOD;
-    left[x]++;
+    //          total[x]--;  
+    // long long leftc = left[2 *x];
+    // long long rightc = total[2 * x];
+    // ans = (ans + (leftc* rightc) % MOD) % MOD;
+    // left[x]++;
 
+    //     }
+    //     return ans;
+
+
+    ////// optimised one
+
+    unordered_map<int,int>icount;
+    unordered_map<int,int>jcount;
+    int ans=0;
+    for(int x:nums){
+        if(x%2==0){
+            ans=(ans+jcount[x/2])%MOD;
         }
-        return ans;
+        jcount[x]=(jcount[x]+icount[x*2])%MOD;
+        icount[x]++;
+    }
+    return ans;
     }
 };

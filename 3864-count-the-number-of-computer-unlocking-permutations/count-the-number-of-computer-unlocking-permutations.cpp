3d89e51877f9d331418a30d8eafaa21abf0c2a82
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2025-12-10 12:47:08 +0530

    Time: 111 ms (5.00%) | Memory: 110.1 MB (8.50%) - LeetSync

---
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+int MOD=1e9+7;
+    int countPermutations(vector<int>& complexity) {
+        set<int>myset;
+        int n=complexity.size();
+        bool mainflag=true;
+        myset.insert(complexity[0]);
+        for(int i=1;i<complexity.size();i++){
+            bool flag=false;
+            int t=complexity[i];
+            for(auto it=myset.rbegin();it!=myset.rend();it++){
+                if(*it<t){
+                    flag=true;
+                    break;
+                }
+            }
+            if(flag==false){
+                return 0;
+            }
+            myset.insert(t);
+        }
+        long long fact = 1;
+        for (int i = 1; i < n; i++) {
+            fact = (fact * i) % MOD;  
+        }
+
+        return (int)fact;
+
+    }
+};

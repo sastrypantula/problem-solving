563f448739c57eea28ee53cdc9b1ae7e59f4d3af
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2025-12-10 17:51:15 +0530

    Time: 6 ms (25.00%) | Memory: 92.9 MB (47.00%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
@@ -2,30 +2,37 @@ class Solution {
 public:
 int MOD=1e9+7;
     int countPermutations(vector<int>& complexity) {
-        set<int>myset;
-        int n=complexity.size();
-        bool mainflag=true;
-        myset.insert(complexity[0]);
-        for(int i=1;i<complexity.size();i++){
-            bool flag=false;
-            int t=complexity[i];
-            for(auto it=myset.rbegin();it!=myset.rend();it++){
-                if(*it<t){
-                    flag=true;
-                    break;
-                }
-            }
-            if(flag==false){
-                return 0;
-            }
-            myset.insert(t);
-        }
-        long long fact = 1;
-        for (int i = 1; i < n; i++) {
-            fact = (fact * i) % MOD;  
-        }
+        // set<int>myset;
+        // int n=complexity.size();
+        // bool mainflag=true;
+        // myset.insert(complexity[0]);
+        // for(int i=1;i<complexity.size();i++){
+        //     bool flag=false;
+        //     int t=complexity[i];
+        //     for(auto it=myset.rbegin();it!=myset.rend();it++){
+        //         if(*it<t){
+        //             flag=true;
+        //             break;
+        //         }
+        //     }
+        //     if(flag==false){
+        //         return 0;
+        //     }
+        //     myset.insert(t);
+        // }
+        // long long fact = 1;
+        // for (int i = 1; i < n; i++) {
+        //     fact = (fact * i) % MOD;  
+        // }
 
-        return (int)fact;
-
+        // return (int)fact;
+        long long res=1;
+    int n=complexity.size();
+    for(int i=1;i<n;i++){
+        if(complexity[i]<=complexity[0]) return 0;
+        res*=i;
+        res=res%MOD;
+    }
+    return res;
     }
 };

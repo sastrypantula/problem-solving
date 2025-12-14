d3a3abe2075f50961a1a935c84f3b5723409ca5d
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2025-12-14 21:56:48 +0530

    Time: 32 ms (41.00%) | Memory: 58.7 MB (26.50%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
@@ -2,31 +2,50 @@ class Solution {
 public:
     int numberOfWays(string corridor) {
         const long long MOD = 1e9 + 7;
-        long long ways = 1;
         int n = corridor.size();
-        int seatCount = 0;
-        long long plantCount = 0;
-        bool countingPlants = false;
+        // int seatCount = 0;
+        // long long plantCount = 0;
+        // bool countingPlants = false;
 
-        for (char c : corridor) {
-            if (c == 'S') {
-                seatCount++;
-                if (seatCount % 2 == 1) {
-                    if (seatCount > 2) {
-                        ways = (ways * (plantCount + 1)) % MOD;
-                    }
-                    plantCount = 0;
-                    countingPlants = false;
-                } else {
-                    countingPlants = true;
-                }
-            } else if (c == 'P' && countingPlants) {
-                plantCount++;
+        // for (char c : corridor) {
+        //     if (c == 'S') {
+        //         seatCount++;
+        //         if (seatCount % 2 == 1) {
+        //             if (seatCount > 2) {
+        //                 ways = (ways * (plantCount + 1)) % MOD;
+        //             }
+        //             plantCount = 0;
+        //             countingPlants = false;
+        //         } 
+        //         else {
+        //             countingPlants = true;
+        //         }
+        //     } else if (c == 'P' && countingPlants) {
+        //         plantCount++;
+        //     }
+        // }
+
+        // if (seatCount < 2 || seatCount % 2 == 1) return 0;
+        // return ways % MOD;
+        int count=0;
+           vector<int> temp;
+        for(int i=0;i<n;i++){
+            if(corridor[i]=='S'){
+                count++;
+                temp.push_back(i);
             }
         }
+        if(count==0 || count==1 || count%2==1){
+            return 0;
+        }
+        long long ways=1;
+        int prev=temp[1];
+        for(int j=2;j<temp.size();j=j+2){
+            ways=((ways)*(temp[j]-prev)%MOD);
+            prev=temp[j+1];
 
-        if (seatCount < 2 || seatCount % 2 == 1) return 0;
-        return ways % MOD;
-
+        }
+    
+    return ways;
     }
 };

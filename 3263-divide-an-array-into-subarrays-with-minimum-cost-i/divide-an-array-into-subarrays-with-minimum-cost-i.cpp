261427cf5d9175d72946a446e31c3b9cb706c417
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2026-02-01 22:28:17 +0530

    Time: 0 ms (100.00%) | Memory: 30.5 MB (74.87%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -11,14 +11,16 @@ public:
        int ans=INT_MAX;
        int n=nums.size();
     
-       for(int i=1;i<n-1;i++){
-        int a=nums[0];
-        int b=nums[i];
-        int c=solve(i+1,n,nums);
-        if(a+b+c<ans){
-            ans=a+b+c;
-        }
-       }
-       return ans;
+    //    for(int i=1;i<n-1;i++){
+    //     int a=nums[0];
+    //     int b=nums[i];
+    //     int c=solve(i+1,n,nums);
+    //     if(a+b+c<ans){
+    //         ans=a+b+c;
+    //     }
+    //    }
+    sort(nums.begin()+1,nums.end());
+     return nums[0]+nums[1]+nums[2];
+    //    return ans;
     }
 };

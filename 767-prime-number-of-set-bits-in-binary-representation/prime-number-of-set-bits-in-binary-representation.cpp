4aa1b68bc931ef1f81952e2b2c3fbf1799a2f74e
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2026-02-21 07:09:00 +0530

    Time: 723 ms (5.00%) | Memory: 8.1 MB (35.12%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
-  bool isPrime(int n) {
-        if (n < 2) return false;
-        for (int i = 2; i * i <= n; ++i) {
-            if (n % i == 0)
-                return false;
-        }
-        return true;
+int chk(int num){
+    if(num==1){
+        return false;
     }
+    for(int i=2;i*i<=num;i++){
+        if(i!=num && num%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+int func(int x){
+int count=0;
+for(int i=0;i<32;i++){
+    if((x & (1<<i))!=0){
+        count++;
+    }
+}
+return count;
 
+}
     int countPrimeSetBits(int left, int right) {
-         int c = 0;
-        for (int i = left; i <= right; ++i) {
-            int bits = __builtin_popcount(i);
-            if (isPrime(bits)) {
-                ++c;
+        int ans=0;
+        for(int i=left;i<=right;i++){
+            int t=func(i);
+            if(chk(t)){
+                ans++;
+                cout<<ans<<"for the i"<<i<<endl;
             }
         }
-        return c;
-
+        return ans;
     }
 };

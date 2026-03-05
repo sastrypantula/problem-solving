d4027cd8b4a921ef350fa4c0d553b4c141ada942
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2026-03-05 18:27:19 +0530

    Time: 3 ms (13.12%) | Memory: 13.5 MB (8.81%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-// copied the solution but understood the Bottom up approach still needs to grind the problem 
-    int t[201][201];
-    int n;
-    int solve(vector<vector<int>>& triangle, int row, int col) {
-        if (row == n-1)
-            return triangle[row][col];
-
-        if(t[row][col] != INT_MAX)
-            return t[row][col];
-        
-        int minPath = triangle[row][col] + 
-                      min(solve(triangle, row + 1, col),
-                          solve(triangle, row + 1, col + 1));
-
-        return t[row][col] = minPath;
+vector<vector<int>> dp;
+int func(int i,int j,vector<vector<int>>& grid){
+    if(i>=grid.size()){
+        return 0;
     }
+      if(dp[i][j]!=INT_MAX){
+        return dp[i][j];
+    }
+   
+    int take_i=grid[i][j]+func(i+1,j,grid);
+    int take_i1=grid[i][j]+func(i+1,j+1,grid);
+    return dp[i][j]=min(take_i,take_i1);
+   
+}
     int minimumTotal(vector<vector<int>>& triangle) {
-        n = triangle.size();
-       
-        for (int i = 0; i < 201; i++) {
-            for (int j = 0; j < 201; j++) {
-                t[i][j] = INT_MAX;
-            }
-        }
-        return solve(triangle, 0, 0);
+       int m=triangle.size();
+       int n=triangle[m-1].size();
+       dp.resize(m,vector<int>(n,INT_MAX));
+       return func(0,0,triangle); 
     }
 };

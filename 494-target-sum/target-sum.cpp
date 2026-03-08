09b28590c4391fab329eea1222ce07a9647d5834
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2026-03-08 18:47:35 +0530

    Time: 18 ms (47.42%) | Memory: 42.8 MB (15.67%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-int ans=0;
+vector<vector<int>> dp;
+int offset=1000;
 int func(int i,int n,vector<int>& nums,int target,int sum){
+    if(sum+offset<0 || sum+offset>2*offset+1){
+        return 0;
+    }
      if(i>=n){
          if(sum==target){
         return 1;
     }
         return 0;
     }
+    if(dp[i][sum+offset]!=-1){
+        return dp[i][sum+offset];
+    }
    int p=func(i+1,n,nums,target,sum+nums[i]);  
    int m=func(i+1,n,nums,target,sum-nums[i]);
-   return p+m;
+   return dp[i][sum+offset]=p+m;
 }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
+        dp.resize(n+1,vector<int>(2*offset+1,-1));
        return func(0,n,nums,target,0);
     }
 };

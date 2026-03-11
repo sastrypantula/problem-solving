29d80d670f95515909ba034a907f9e3de3b9da81
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2026-03-11 12:50:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (59.24%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n==0){
+            return 1;
+        }
+        string s="";
+        while(n>0){
+            int rem=n%2;
+            s.push_back((rem+'0'));
+            n=n/2;
+        }
+        reverse(s.begin(),s.end());
+        for(int i=0;i<s.length();i++){
+              s[i]='1'-s[i]+'0';
+        }
+        // cout<<s<<endl;
+        int num=0;
+        int size=s.length();
+        for(int i=s.length()-1;i>=0;i--){
+            if(s[i]=='1'){
+                num=num+pow(2,size-i-1);
+            }
+        }
+        return num;
+    }
+};

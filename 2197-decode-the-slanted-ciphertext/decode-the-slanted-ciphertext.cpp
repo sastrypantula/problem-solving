a8f2a7f1c12f96499926341d1f9a033d2196fc3f
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2026-04-04 13:42:42 +0530

    Time: 40 ms (42.37%) | Memory: 49.1 MB (21.19%) - LeetSync

--- 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
@@ -1,16 +1,59 @@
+// class Solution {
+// public:
+//     string decodeCiphertext(string encodedText, int rows) {
+//         string res="";
+//         int cols=encodedText.length()/rows;
+
+//        for(int col=0;col<cols;col++){
+//         for(int j=col;j<encodedText.length();j=j+cols+1){
+//             res.push_back(encodedText[j]);
+//         }
+//        }
+//       int n1=res.length();
+//      int k=n1-1;
+//       while(k>=0 && res[k]==' '){
+//         res.pop_back();
+//         k--;
+//       }
+//       return res;
+//     }
+// };
 class Solution {
 public:
     string decodeCiphertext(string encodedText, int rows) {
         string res="";
         int cols=encodedText.length()/rows;
-
-       for(int col=0;col<cols;col++){
-        for(int j=col;j<encodedText.length();j=j+cols+1){
-            res.push_back(encodedText[j]);
+        vector<vector<char>> temp(rows,vector<char>(cols,'.'));
+        int k=0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++,k++){
+                if(encodedText[k]==' '){
+                    continue;
+                }
+                temp[i][j]=encodedText[k];
+            }
         }
+
+        int count2=0;
+        while(count2<cols){
+            int i=0;
+            int j=count2;
+       int count1=0;
+       while(count1<rows && i<rows && j<cols){
+        if(temp[i][j]=='.'){
+            res.push_back(' ');
+        }
+        else{
+         res.push_back(temp[i][j]);
+        }
+        i++;
+        j++;
+        count1++;
        }
-      int n1=res.length();
-     int k=n1-1;
+       count2++;
+        }
+      int n=res.length();
+      k=n-1;
       while(k>=0 && res[k]==' '){
         res.pop_back();
         k--;

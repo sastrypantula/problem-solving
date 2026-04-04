0e17a6318d8fe7847196b695fba102bb2e9582ae
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2026-04-04 13:41:39 +0530

    Time: 45 ms (27.12%) | Memory: 45.9 MB (48.30%) - LeetSync

--- 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
@@ -1,81 +1,20 @@
 class Solution {
 public:
     string decodeCiphertext(string encodedText, int rows) {
-//         int cols=encodedText.length()/rows;
-//         vector<vector<char>>result(rows,vector<char>(cols,'.'));
-//         int n=encodedText.length();
-//         int k=0;
-//         bool flag=false;
-//         int temp=0;
-     
-//         for(int i=0;i<rows;i++){
-//             for(int j=0;j<cols;j++){
-//                 if(encodedText[k]!=' '){
-//                     result[i][j]=encodedText[k];
-//                     flag=true;
-//                     temp++;
-//                 }
-//                 cout<<result[i][j]<<" ";   
-//                 k++;
-//             }
-//             cout<<endl;
-//         }
-//            if(flag==false){
-//             return encodedText;
-//            }
-//            string ans="";
-//            int i=0;
-//             for(int j=0;j<cols;j++){
-//                 int count=0;
-//                 int i1=i;
-//                 int j1=j;
-//                 while(count<rows){
-//                     if(i1>=rows || j1>=cols){
-//                         break;
-//                     }
-//                     if(result[i1][j1]!='.'){
-//                         ans.push_back(result[i1][j1]);
-//                     }
-//                     if(result[i1][j1]=='.'){
-//                         ans=ans+" ";
-//                     }
-//                     count++;
-//                     i1++;
-//                     j1++;
+        string res="";
+        int cols=encodedText.length()/rows;
 
-//                 }
-//             }
-           
-//  while (!ans.empty() && ans.back() == ' ') {
-//             ans.pop_back();
-//         }
-
-              
-//            return ans;
-
-
-int n = encodedText.length();
-        if (n == 0 || rows == 0) return "";
-
-        int cols = n / rows;
-        string ans = "";
-
-        for (int j = 0; j < cols; ++j) {
-            int i = 0, j1 = j;
-            while (i < rows && j1 < cols) {
-                char ch = encodedText[i * cols + j1];
-                ans.push_back(ch);
-                ++i;
-                ++j1;
-            }
+       for(int col=0;col<cols;col++){
+        for(int j=col;j<encodedText.length();j=j+cols+1){
+            res.push_back(encodedText[j]);
         }
-
-        // Trim trailing spaces
-        while (!ans.empty() && ans.back() == ' ') {
-            ans.pop_back();
-        }
-
-        return ans;
-
+       }
+      int n1=res.length();
+     int k=n1-1;
+      while(k>=0 && res[k]==' '){
+        res.pop_back();
+        k--;
+      }
+      return res;
     }
 };

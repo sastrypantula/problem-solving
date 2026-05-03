4d6b4b2f1d93f5dc861bd62e0e7f22aaea69ae0e
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2026-05-03 13:16:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (45.86%) - LeetSync

---
+++ 804-rotated-digits/rotated-digits.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int rotatedDigits(int n) {
+        int count=0;
+        for(int i=1;i<=n;i++){
+                int temp=i;
+                bool flag=false;
+                while(temp>0){
+                    int rem=temp%10;
+                   if(rem==2 || rem==5 || rem==6 || rem==9){
+                    flag=true;
+                    
+                   }
+                   if(rem==3 || rem==4 || rem==7){
+                    flag=false;
+                    break;
+                   }
+                   temp=temp/10;
+                }
+                if(flag) count++;
+            }
+        
+        return count;
+    }
+};

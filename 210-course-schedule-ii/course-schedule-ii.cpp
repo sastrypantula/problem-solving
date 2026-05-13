36b89d3bf50177633f6611cf26574a3aacc8c90b
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2026-05-13 17:51:57 +0530

    Time: 11 ms (12.12%) | Memory: 19.4 MB (10.70%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,62 +1,42 @@
 class Solution {
 public:
-bool isCycle(int src,vector<bool>& visited,vector<bool>& recpath,vector<vector<int>>& prerequisites){
-    visited[src]=true;
-    recpath[src]=true;
-    for(int i=0;i<prerequisites.size();i++){
-        int u=prerequisites[i][1];
-        int v=prerequisites[i][0];
-        if(u==src){
-            if(!visited[v]){
-                if(isCycle(v,visited,recpath,prerequisites)){
-                    return true;
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,vector<int>> mymap;
+        int n=numCourses;
+        for(int i=0;i<prerequisites.size();i++){
+            int u=prerequisites[i][1];
+             int v=prerequisites[i][0];
+             mymap[u].push_back(v);
+        }
+        vector<int> indegree(n,0);
+        for(auto it:mymap){
+            vector<int> adj=it.second;
+            for(auto v:adj){
+                indegree[v]++;
+            }
+        }
+        queue<int> que;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                que.push(i);
+            }
+        }
+        vector<int> res;
+  
+        while(!que.empty()){
+            int u=que.front();
+            que.pop();
+                  res.push_back(u);
+            for(auto v:mymap[u]){
+                indegree[v]--;
+                if(indegree[v]==0){
+                    que.push(v);
                 }
             }
-            else{
-              if(recpath[v]){
-                return true;
-              }  
-            }
         }
-    }
-    recpath[src]=false;
-    return false;
-}
-
-void topo(int src,vector<bool>& visi2,vector<vector<int>>& prerequisites,stack<int>& s){
-visi2[src]=true;
-for(int i=0;i<prerequisites.size();i++){
-    int u=prerequisites[i][1];
-    int v=prerequisites[i][0];
-    if(src==u){
-        if(!visi2[v]){
-            topo(v,visi2,prerequisites,s);
-        }
-    }
-}
-s.push(src);
-}
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-           int V=numCourses;
-           vector<int>res;
-        vector<bool>visited(V,false);
-        vector<bool>recpath(V,false);
-        for(int i=0;i<V;i++){
-
-        if(isCycle(i,visited,recpath,prerequisites)){
+        if(res.size()==n){
             return res;
         }
-        }
-        vector<bool>visi2(V,false);
-        stack<int>s;
-        for(int i=0;i<V;i++){
-            if(!visi2[i])
-           topo(i,visi2,prerequisites,s);
-        }
-        while(s.size()>0){
-            res.push_back(s.top());
-            s.pop();
-        }
-        return res;
+        return {};
     }
 };

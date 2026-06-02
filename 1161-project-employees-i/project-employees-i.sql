69e70c1eac6102e6f3fe32f52b694b6a0a8f90d6
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2026-06-02 11:42:44 +0530

    Time: 740 ms (5.77%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select temp.project_id,round(AVG(temp.experience_years),2) as average_years from (select project_id,experience_years from Project join Employee on Project.employee_id=Employee.employee_id) as temp group by project_id;

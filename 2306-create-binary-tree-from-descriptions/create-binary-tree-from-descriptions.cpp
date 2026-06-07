72b2675925ce282fd7ef674a4225cffcb4baef76
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2026-06-07 15:09:03 +0530

    Time: 406 ms (6.69%) | Memory: 339.2 MB (5.12%) - LeetSync

---
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,72 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   unordered_map<int,vector<int>> mymap;
+        unordered_map<int,int> freq;
+        TreeNode* func(int r,int v){
+            TreeNode* root=new TreeNode(r);
+            int a=-1;
+            int b=-1;
+            if(mymap.count(r) && mymap[r].size()>1){
+                if(freq[mymap[r][0]]==1){
+                    a=mymap[r][0];
+                    b=mymap[r][1];
+                }
+                else{
+                    a=mymap[r][1];
+                    b=mymap[r][0];
+                }
+            }
+            if(mymap.count(r) && mymap[r].size()==1){
+                if(freq[mymap[r][0]]==1){
+                    a=mymap[r][0];
+                }
+                else{
+                    b=mymap[r][0];
+                }
+            }
+            if(a==-1){
+                root->left=NULL;
+            }
+            else{
+                  root->left=func(a,1);
+            }
+            if(b==-1){
+                root->right=NULL;
+            }
+            else{
+                root->right=func(b,0);
+            }
+            return root;
+        }
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+     
+        int root=0;
+        for(auto it:descriptions){
+            int a=it[0];
+            int b=it[1];
+            int c=it[2];
+            mymap[a].push_back(b);
+            freq[b]=c;
+        }
+        for(auto it:mymap){
+            int key=it.first;
+            if(!freq.count(key)){
+                root=key;
+                break;
+            }
+        }
+        return func(root,-1);
+        
+    }
+};

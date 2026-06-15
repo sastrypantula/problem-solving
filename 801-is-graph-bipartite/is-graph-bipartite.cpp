1c8088a17ac7202fc6e379e34699ddd289ba8279
SIVARAMASASTRY <147686159+sastrypantula@users.noreply.github.com>
2026-06-15 15:28:37 +0530

    Time: 12 ms (5.00%) | Memory: 20.7 MB (5.15%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
    unordered_map<int,vector<int>> mymap;
-   bool dfs(int i,vector<int>& color,int curr){
+//    bool dfs(int i,vector<int>& color,int curr){
 
-    color[i]=curr;
-    for(auto it:mymap[i]){
-        if(color[it]==-1){
-            if(dfs(it,color,1-curr)==false){
-                return false;
-            }
-        }
-        else if(color[it]==curr){
-            return false;
-        }
-    }
-    return true;
-   }
+//     color[i]=curr;
+//     for(auto it:mymap[i]){
+//         if(color[it]==-1){
+//             if(dfs(it,color,1-curr)==false){
+//                 return false;
+//             }
+//         }
+//         else if(color[it]==curr){
+//             return false;
+//         }
+//     }
+//     return true;
+//    }
     bool isBipartite(vector<vector<int>>& graph) {
      
         for(int i=0;i<graph.size();i++){
@@ -25,11 +25,38 @@ public:
         }
         int n=graph.size();
         vector<int> color(n,-1);
+        // for(int i=0;i<n;i++){
+        //     if(color[i]==-1){
+        //         if(dfs(i,color,0)==false){
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
+        
+        //BFS approach 
         for(int i=0;i<n;i++){
             if(color[i]==-1){
-                if(dfs(i,color,0)==false){
+        queue<int> q;
+        q.push(i);
+        color[i]=0;
+        while(!q.empty()){
+            int n=q.size();
+            while(n--){
+            int node=q.front();
+            q.pop();
+
+            for(auto& it:mymap[node]){
+                if(color[it]==-1){
+                    q.push(it);
+                    color[it]=1-color[node];
+                }
+                else if(color[it]==color[node]){
                     return false;
                 }
+            }
+            }
+        }
             }
         }
         return true;
